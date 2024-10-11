65d5155db558544280d45eed139c7fb3958c6fd5
Sayyam <sayyambutt317@gmail.com>
2024-10-11 03:30:35 -0700

    update card effects and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
 import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
 <main className=" main-h-screen bg-black/[0.96] antialised bg-grid-white/[0.02]">
-<HeroSection />
+{/* <HeroSection /> */}
+<Instructor/>
 <FeaturedCourses/>
 <WhyChooseUs/>
 <TestimonialCard/>
+<UpcomingWebinars/>
+<Instructor/>
 </main>
   );
 }
